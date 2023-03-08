2764f2483a72e90188ebbadbe87ecfdec88646f9
Reynanwq <reynanwq@gmail.com>
2023-03-07 22:40:52 -0300

    criando pasta src

--- src/menu.js
+++ src/menu.js
@@ -5,10 +5,10 @@ var menu = document.getElementById('menu');
 menuBotao.onclick = function() {
     if (navegar.style.right == "-250px") {//deixa a area de nav fechada 
         navegar.style.right = "0";//se for aberta vai trocar a img do menu
-        menu.src = "./assets/close.png";
+        menu.src = "./src/assets/close.png";
     }
     else {
         navegar.style.right = "-250px";//se não ela vai estar fechada e mantém a img original do menu
-        menu.src = "./assets/menu.png";
+        menu.src = "./src/assets/menu.png";
     }
 }   
